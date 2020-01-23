7b1bdd7012683a8227974efd348e06d1e4559a3f
Zach Forrest <ZachForrest@Zachs-MacBook-Pro.local>
2020-01-23 14:17:30 -0500

    added structure to html and set up .gitignore

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  
+
+  // let user = $.ajax( "https://api.github.com/users/zforre" )
+  //   .done((resp) => {
+  //     console.log(resp.login);
+  //   })
+  //   .fail((err) => {
+  //     console.log(err);
+  //   })
+  //   .always(()=> {
+  //     console.log('always fires');
+  //   });
+
+});
