5f34e99abc177de0572a646dfe5b4dcdceb0f516
Zach Forrest <ZachForrest@Zachs-MacBook-Pro.local>
2020-01-24 14:25:06 -0500

    continues to add style to html elements

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
-  $.ajax(`https://api.github.com/users/zforre?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`)
-  .done((resp) => {
-    // updateRepos(resp);
-  })
-  .fail((err) => {
-    console.log(err);
-  })
-  .always(() => {
-    console.log('always fires');
-  });
+  // $.ajax(`https://api.github.com/users/zforre?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`)
+  // .done((resp) => {
+  //   // updateRepos(resp);
+  // })
+  // .fail((err) => {
+  //   console.log(err);
+  // })
+  // .always(() => {
+  //   console.log('always fires');
+  // });
 
 });
