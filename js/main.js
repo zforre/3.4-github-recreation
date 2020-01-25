818ddf0a82aa528950014b5c4f143b11ac8adcb0
Zach Forrest <ZachForrest@Zachs-MacBook-Pro.local>
2020-01-25 13:59:30 -0500

    bringing in content from github api

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  'use strict';
 
   // $.ajax(`https://api.github.com/users/zforre?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`)
   // .done((resp) => {
@@ -10,5 +11,54 @@ $(document).ready(function() {
   // .always(() => {
   //   console.log('always fires');
   // });
+
+  //userName
+
+
+
+
+
+  $.ajax({
+    url: `https://api.github.com/users/zforre?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
+    datatype: "jsonp",
+    method: "GET",
+    success: function(response) {
+      console.log("SUCCESS", response);
+      user(response);
+    }
+  });
+
+  function user(response) {
+    console.log("hello user")
+    var source = $('#side-user-script').html();
+    var template = Handlebars.compile(source);
+    var context = response;
+    var html = template(context);
+    $('#side-user').html(html);
+  }
+
+  $.ajax({
+    url: `https://api.github.com/users/zforre/repos?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
+    datatype: "jsonp",
+    method: "GET",
+    success: function(response) {
+      console.log("SUCCESS", response);
+      add(response);
+    }
+  });
+
+  function add(response) {
+    console.log("hello repos")
+    let source = $('#repos-script').html();
+    let template = Handlebars.compile(source);
+    let context = response;
+    let html = template(context);
+    $('#repos').html(html);
+  }
+
+
+
+
+
 
 });
